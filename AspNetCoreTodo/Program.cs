d3885aa7386308e1e763569ab4feb155850ec019
Pujan Bajra <pujanisme@gmail.com>
2019-04-24 17:53:28 +0545

    Added adminactions partial page

--- AspNetCoreTodo/Program.cs
+++ AspNetCoreTodo/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace AspNetCoreTodo
@@ -15,6 +16,23 @@ namespace AspNetCoreTodo
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+        }
+
+        private static void InitializeDatabase(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    SeedData.InitializeAsync(services).Wait();
+                }                catch (Exception ex)
+                {
+                    var logger = services
+                    .GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Error occurred seeding the DB.");
+                }
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
